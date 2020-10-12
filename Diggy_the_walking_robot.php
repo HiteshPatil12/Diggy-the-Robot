e462f3883a5cd2bbe09988225fcd4b88b2df575d
Hitesh <36270504+HiteshPatil12@users.noreply.github.com>
2020-10-13 03:00:11 +0530

    Update Diggy_the_walking_robot.php

--- Diggy_the_walking_robot.php
+++ Diggy_the_walking_robot.php
@@ -3,8 +3,7 @@
 const NORTH = 1;
 const EAST = 2;
 const SOUTH = 3;
-const WEST = 4;
-
+const WEST = 4;                                                                 //setting constant values for the directions
 $direction = [1 => "NORTH", 2 => "EAST", 3 => "SOUTH", 4 => "WEST"];
 
 const MULTIPLIER_1 = 1;
@@ -13,24 +12,22 @@ const MULTIPLIER_3 = - 1;
 const MULTIPLIER_4 = - 1;
 
 $x = $argv[1];
-$y = $argv[2];
-
+$y = $argv[2];                                                                  //here using $argv to read input from command line
 if (!is_numeric($x) || !is_numeric($y))
 {
     die("\nCo-ordinates must be Integer\n");
-}
-
+}                                                                               //checking condition whether input in feasible
 $presentDirection = $argv[3];
 
 if ($presentDirection != 'NORTH' && $presentDirection != 'EAST' && $presentDirection != 'SOUTH' && $presentDirection != 'WEST')
 {
     die("\nWrong Direction\n");
-}
-
+}                                                                               //checking condition whether input in feasible
 $presentDirectionNumber = constant($presentDirection);
-echo "PDN_" . $presentDirectionNumber . "\n";
+// echo "PDN_" . $presentDirectionNumber . "\n";
 $path = $argv[4];
-echo "path_" . $path . "\n";
+// echo "path_" . $path . "\n";
+
 for ($i = 0;$i < strlen($path);$i++)
 {
 
@@ -44,8 +41,9 @@ for ($i = 0;$i < strlen($path);$i++)
             else
             {
                 $presentDirectionNumber++;
-                echo "R_switch_" . $presentDirectionNumber . "\n";
-            }
+                // echo "R_switch_" . $presentDirectionNumber . "\n";
+                
+            }                                                                   //to make the rotation clockwise 90 degree
         break;
 
         case 'L':
@@ -56,22 +54,25 @@ for ($i = 0;$i < strlen($path);$i++)
             else
             {
                 $presentDirectionNumber--;
-                echo "L_switch_" . $presentDirectionNumber . "\n";
-            }
+                // echo "L_switch_" . $presentDirectionNumber . "\n";
+                
+            }                                                                   //to make the rotation counter-clockwise 90 degree
         break;
 
         case 'W':
             if (!($presentDirectionNumber % 2))
             {
                 $x += ($path{$i + 1} * constant("MULTIPLIER_" . $presentDirectionNumber));
+                // echo $x;
+                
             }
             else
             {
                 $y += ($path{$i + 1} * constant("MULTIPLIER_" . $presentDirectionNumber));
             }
-            echo "W_switch_" . $presentDirectionNumber . "\n";
-            echo "X:" . $x . "\n";
-            echo "Y:" . $y . "\n";
+            // echo "W_switch_" . $presentDirectionNumber . "\n";
+            // echo "X:" . $x . "\n";
+            // echo "Y:" . $y . "\n";
             $i++;
         break;
 
